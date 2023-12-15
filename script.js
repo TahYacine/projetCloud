ba9ebd47172ccdffc8088fcd8acf666eee60115d
Yacine Tahir <yacine.tahir@uha.fr>
2023-12-15 08:40:17 +0100

    a

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2ZVJw5Xjl6EJSJUK-_MwWwToMX1NNRpM",
-  authDomain: "tpcloudhtml.firebaseapp.com",
-  projectId: "tpcloudhtml",
-  storageBucket: "tpcloudhtml.appspot.com",
-  messagingSenderId: "391306792285",
-  appId: "1:391306792285:web:73b143c1e156c28f7dacb4",
-  measurementId: "G-HW99B20Y4R"
+  apiKey: "AIzaSyBT1y6ZcfuAvRAWnjgUSYqie3zkxs4IhFY",
+  authDomain: "projetfirebasecloud.firebaseapp.com",
+  projectId: "projetfirebasecloud",
+  storageBucket: "projetfirebasecloud.appspot.com",
+  messagingSenderId: "263860026087",
+  appId: "1:263860026087:web:2cdbb1839bb5b952790042",
+  measurementId: "G-63B1SVXLFZ"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
